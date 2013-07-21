5ba1e130176a50ab605bd9e2cd86b22c9d8e1fd1
Andrew Nikolaenko <andrewcgg@yahoo.com>
2013-07-21 23:56:54 +0300

    Escape titles inside tags in WP plugin

--- js/contextly-wordpress.js
+++ js/contextly-wordpress.js
@@ -451,7 +451,12 @@ Contextly.SnippetWidgetFormatter = Contextly.createClass({
     },
 
     escape: function ( text ) {
-        return escape( text );
+        return text
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
     }
 });
 
