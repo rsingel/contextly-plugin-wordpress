6fe25cd86e5f25418e0c23b3629b1c6a7874c903
shotaK <sh.qarqashadze@yahoo.com>
2014-05-17 19:35:57 +0400

    set interval for responsive function to make sure all extra links are removed

--- js/contextly-wordpress.js
+++ js/contextly-wordpress.js
@@ -970,42 +970,22 @@ Contextly.SnippetWidgetFormatter = Contextly.createClass({
             return content;
         }
 
-        function ctxCheckIfWidgetLoadedAndResize() {
-            documentLoadCheckCount++;
-
+        jQuery(window).resize(function() {
             ctxResponsiveResizeHandler();
-            ctxClearIfWidgetLoadedInterval();
-
-            if ( documentLoadCheckCount > 10 ) {
-                ctxClearIfWidgetLoadedInterval();
-            }
-        }
-
-        function ctxClearIfWidgetLoadedInterval() {
-            if ( documentLoadInterval ) {
-                clearInterval( documentLoadInterval );
-            }
-        }
-
-        jQuery(window).resize(
-            function() {
-                ctxResponsiveResizeHandler();
-            }
-        );
+        });
 
         var documentLoadInterval = null;
         var documentLoadCheckCount = 0;
 
-        jQuery(document).ready(
-            function() {
-                documentLoadInterval = self.setInterval(
-                    function () {
-                        ctxCheckIfWidgetLoadedAndResize();
-                    },
-                    500
-                );
+        documentLoadInterval = self.setInterval(function () {
+            documentLoadCheckCount++;
+
+            ctxResponsiveResizeHandler();
+
+            if ( documentLoadCheckCount > 5 ) {
+                clearInterval( documentLoadInterval );
             }
-        );
+        }, 500 );
     }
 
 });
