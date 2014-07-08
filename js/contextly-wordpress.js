2b57fc28ec1c80c3a69768420c647a2a8ae0e674
Andrew Nikolaenko <andrew@anikolaenko.com>
2014-07-08 14:01:28 +0300

    Remove strange characters in sidebar HTML formatter

--- js/contextly-wordpress.js
+++ js/contextly-wordpress.js
@@ -1451,7 +1451,7 @@ Contextly.SidebarWidgetFormatter = Contextly.createClass({
         return "<a href=\"" +
             this.escape( link.native_url ) + "\" title=\"" +
             this.escape( link.title ) + "\" class='ctx-clearfix ctx-nodefs ctx-no-images' onmousedown=\"this.href='" +
-            this.escape( link.url ) + "'\" " + this.getOnclickHtml( link ) + ">" + " " + content + "</a>";
+            this.escape( link.url ) + "'\" " + this.getOnclickHtml( link ) + ">" + content + "</a>";
     },
 
     getLinkHTML: function ( link ) {
