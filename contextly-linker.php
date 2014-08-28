17ff496ab8c9ef87800c28e1f140c10020ef3fd7
Andrew Nikolaenko <andrew@anikolaenko.com>
2014-08-28 10:09:12 +0300

    Update plugin description

--- contextly-linker.php
+++ contextly-linker.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Contextly
 Plugin URI: http://contextly.com
-Description: Adds the Contextly related links tool to your blog. Contextly lets you create related links that helps your readers find more to read, increases your page views and shows off your best content.
+Description: Adds the Contextly content recommendation tool to your blog. Contextly provides related, trending, evergreen and personalized recommendations to help your reader keep reading. Includes text, video and product recommendations to show off your best content.
 Author: Contextly
 Version: 1.3.1
 */
