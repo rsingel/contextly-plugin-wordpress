1363d47f4d5fe40a628077c4dcb8e977bd023285
Andrew Nikolaenko <andrew@anikolaenko.com>
2014-08-29 23:38:23 +0300

    Update last live version number

--- contextly-linker.php
+++ contextly-linker.php
@@ -4,10 +4,10 @@ Plugin Name: Contextly
 Plugin URI: http://contextly.com
 Description: Adds the Contextly content recommendation tool to your blog. Contextly provides related, trending, evergreen and personalized recommendations to help your reader keep reading. Includes text, video and product recommendations to show off your best content.
 Author: Contextly
-Version: 1.3.1
+Version: 1.3.2
 */
 
-define ( "CONTEXTLY_PLUGIN_VERSION", '1.3.1' );
+define ( "CONTEXTLY_PLUGIN_VERSION", '1.3.2' );
 define ( "CONTEXTLY_MODE", 'live' );
 define ( "CONTEXTLY_HTTPS", is_ssl() );
 
