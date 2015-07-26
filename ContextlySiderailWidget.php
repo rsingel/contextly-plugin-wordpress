fd52c5be342910407c6efc5878423f31c16722a4
Dmitriy.trt <dmitriy.trt@gmail.com>
2015-07-27 02:13:45 +0600

    Side Rail support.

---
+++ ContextlySiderailWidget.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Adds Siderail widget.
+ */
+class ContextlyWpSiderailWidget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'contextly_siderail_widget', // Base ID
+			__( 'Contextly Side Rail', 'contextly_linker_textdomain' ), // Name
+			array( 'description' => __( 'Displays links recommended by Contextly.', 'contextly_linker_textdomain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		if ( !$GLOBALS['contextly']->isLoadWidget() )
+		{
+			return;
+		}
+
+		print $args['before_widget'];
+		print '<div class="ctx-siderail-container"></div>';
+		print $args['after_widget'];
+	}
+
+} // class ContextlyWpSiderailWidget
