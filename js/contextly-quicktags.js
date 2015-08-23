69eb5554b319f86a0b8b12a666b369049543b6ed
Dmitriy.trt <dmitriy.trt@gmail.com>
2015-08-23 21:03:18 +0600

    Auto-sidebar support. Update QTags editor on sidebar changes.

--- js/contextly-quicktags.js
+++ js/contextly-quicktags.js
@@ -1,29 +1,5 @@
 (function( $ ) {
 
-    var buildSidebarRegexp = function (id, modifiers) {
-        var pattern = '\\[contextly(?:_auto)?_sidebar\\s+id="';
-        if (id) {
-            pattern += id;
-        }
-        else {
-            pattern += '([^"\\]]+)';
-        }
-        pattern += '"\\s*\\]';
-        modifiers = modifiers || 'i';
-        return new RegExp(pattern, modifiers);
-    };
-
-    var buildSidebarToken = function (sidebar) {
-        // Build the token code.
-        var token = '[contextly';
-        if (sidebar.type == Contextly.widget.types.AUTO_SIDEBAR)
-        {
-            token += '_auto';
-        }
-        token += '_sidebar id="' + sidebar.id + '"]';
-        return token;
-    };
-
     function getSelectedText( canvas )
     {
         if ( document.selection ) // IE
@@ -46,6 +22,7 @@
             {
                 registerContextlyQtLinkButton();
                 registerContextlyQtSidebarButton();
+				registerContextlyQtEventHandlers();
             }
         }
     );
@@ -81,17 +58,20 @@
             'ctx_sidebar',
             'Contextly Sidebar',
             function(e, c, ed) {
-                var sidebar_id = null;
+                var sidebar_id = null, sidebar_type = Contextly.widget.types.SIDEBAR;
 
                 // Try to extract existing sidebar ID from the editor.
                 var selection_text = getSelectedText( c );
-                var matches = selection_text.match(buildSidebarRegexp());
+                var matches = selection_text.match(Contextly.PostEditor.buildSidebarRegexp());
                 if (matches) {
-                    sidebar_id = matches[1];
+					sidebar_id = matches[2];
+					if (matches[1]) {
+						sidebar_type = Contextly.widget.types.AUTO_SIDEBAR;
+					}
                 }
 
-                Contextly.PostEditor.sidebarPopup(sidebar_id, function (sidebar) {
-                    var token = buildSidebarToken(sidebar);
+                Contextly.PostEditor.sidebarPopup(sidebar_id, sidebar_type, function (sidebar) {
+                    var token = Contextly.PostEditor.buildSidebarToken(sidebar);
                     QTags.insertContent( token );
                 });
             },
@@ -101,5 +81,76 @@
             500
         );
     }
+
+	// Adds unique namespace to the event type.
+	function eventNamespace(type) {
+		type = type || '';
+		return type + '.contextlyQTags';
+	}
+
+	function updateEditorContent(callback)
+	{
+		if ( Contextly.PostEditor.isWysiwygActive() )
+		{
+			return;
+		}
+
+		var textarea = $('#content');
+		if ( !textarea.length )
+		{
+			return;
+		}
+
+		var content = textarea.val() || '';
+		var originalLength = content.length;
+		content = callback( content );
+		if ( content.length !== originalLength )
+		{
+			textarea.val(content);
+		}
+	}
+
+	// The widget removal handler.
+	function onWidgetRemoved(e, widgetType, id, widget) {
+		switch (widgetType) {
+			case Contextly.widget.types.AUTO_SIDEBAR:
+				// Remove the auto-sidebar shortcode ignoring the ID.
+				id = null;
+				// no break;
+
+			case Contextly.widget.types.SIDEBAR:
+				updateEditorContent(function(content) {
+					var regexp = Contextly.PostEditor.buildSidebarRegexp(id, widgetType, 'ig');
+					return content.replace(regexp, '');
+				});
+				break;
+		}
+	}
+
+	// The widget update handler.
+	function onWidgetUpdated(e, widgetType, id, widget) {
+		switch (widgetType) {
+			case Contextly.widget.types.AUTO_SIDEBAR:
+				updateEditorContent(function(content) {
+					var regexp = Contextly.PostEditor.buildSidebarRegexp(null, widgetType, 'ig');
+					var token = Contextly.PostEditor.buildSidebarToken(widget);
+					return content.replace(regexp, token);
+				});
+
+				break;
+		}
+	}
+
+	function registerContextlyQtEventHandlers()
+	{
+		var eventHandlers = {
+			'contextlyWidgetRemoved': onWidgetRemoved,
+			'contextlyWidgetUpdated': onWidgetUpdated
+		};
+		var $window = $(window);
+		for (var key in eventHandlers) {
+			$window.bind(eventNamespace(key), eventHandlers[key]);
+		}
+	}
 
 })( jQuery );
